caa68cad12e66dd71c36ad7fccc86b2280d4ff50
Regard712 <2727117436@qq.com>
2025-04-14 22:45:02 +0800

    cstudy

--- test/test/test.c
+++ test/test/test.c
@@ -1,7 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
+
+#include<stdio.h>
 int main()
 {
+	int num = 0;
+	scanf("%d", &num);
+	if (num > 0)
+	{
+		if (num % 2 == 0)
+		{
+			printf("%d是偶数\n", num);
+		}
+		else
+		{
+			printf("%d是奇数\n", num);
+		}
+	}
+	else
+	{
+		printf("非奇数\n");
+	}
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
